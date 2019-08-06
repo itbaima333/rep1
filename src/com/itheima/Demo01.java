73612373f2cf307124898c7aa35b77bf5d80974e
liyunlong <dulituan@qq.com>
2019-08-06 18:32:50 +0800

    第一次提交

---
+++ src/com/itheima/Demo01.java
@@ -0,0 +1,23 @@
+package com.itheima;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		 
+		
+		int i=10;
+		int j=20;
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println(i+j);
+		
+		
+	}
+
+}
